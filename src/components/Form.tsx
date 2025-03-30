0715bb323f1b4fae6f011e8210546a763596be67
Tanja Veselinovic <tanjaveselinovic@Tanjas-MacBook-Pro.local>
2025-03-30 18:49:29 +0200

    React Hook Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,103 +5,41 @@ import ServicesDropdown from "./ServicesDropdown";
 import AvailableTimesDropdown from "./AvailableTimesDropdown";
 import { bookAppointment } from "../API/api";
 import barberImage from "../assets/Image.jpg";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-const validateForm = ({
-  email,
-  firstName,
-  lastName,
-  phone,
-  selectedBarber,
-  selectedService,
-  selectedTime,
-  selectedDate,
-}: {
+type FormData = {
   firstName: string;
   lastName: string;
   email: string;
   phone: string;
-  selectedTime: string;
   selectedBarber: string;
   selectedService: string;
+  selectedTime: string;
   selectedDate: string;
-}): {
-  errors: Record<string, string>;
-  errorCount: number;
-  success: boolean;
-} => {
-  let newErrors: { [key: string]: string } = {};
-  if (!firstName || !lastName) {
-    newErrors.name = "Please enter your full name";
-  }
-
-  if (!email || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-    newErrors.email = "Please enter a valid email";
-  }
-
-  if (!phone || !/^(\+386\s?[1-7]\s?\d{3}\s?\d{2}\s?\d{2})$/.test(phone)) {
-    newErrors.phone = "Please enter a valid phone number";
-  }
-
-  if (!selectedBarber) {
-    newErrors.barber = "Please select a barber";
-  }
-
-  if (!selectedService) {
-    newErrors.service = "Please select a service";
-  }
-
-  if (selectedDate !== "") {
-    newErrors.date = "Please select a date";
-  }
-
-  if (!selectedTime) {
-    newErrors.time = "Please select a time";
-  }
-
-  const errorCount = Object.keys(newErrors).length;
-  return {
-    errors: newErrors,
-    errorCount,
-    success: errorCount === 0,
-  };
 };
 
 function Form() {
-  const [selectedBarber, setSelectedBarber] = useState<string>("");
-  const [selectedDate, setSelectedDate] = useState<string>("");
   const [servicePrice, setServicePrice] = useState<string>("");
-  const [selectedService, setSelectedService] = useState<string>("");
-  const [selectedTime, setSelectedTime] = useState<string>("");
-  const [errors, setErrors] = useState<{ [key: string]: string }>({});
   const navigate = useNavigate();
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+  } = useForm<FormData>();
 
-    const formData = new FormData(event.currentTarget as HTMLFormElement);
+  const selectedDate = watch("selectedDate");
+  const selectedBarber = watch("selectedBarber");
 
-    const validationResult = validateForm({
-      firstName: formData.get("firstName")?.toString().trim() || "",
-      lastName: formData.get("lastName")?.toString().trim() || "",
-      email: formData.get("email")?.toString().trim() || "",
-      phone: formData.get("phone")?.toString().trim() || "",
-      selectedBarber: formData.get("barber")?.toString() || "",
-      selectedService: formData.get("service")?.toString() || "",
-      selectedDate: formData.get("date")?.toString() || "",
-      selectedTime: formData.get("time")?.toString() || "",
-    });
-    console.log(validationResult);
-    setErrors(validationResult.errors);
-
-    if (!validationResult.success) {
-      console.log("Form has errors:", validationResult.errors);
-      return;
-    }
-
+  const onSubmit: SubmitHandler<FormData> = (data) => {
     bookAppointment({
-      startDate: new Date(`${selectedDate}T${selectedTime}`).getTime(),
-      barberId: selectedBarber,
-      serviceId: selectedService,
+      startDate: new Date(
+        `${data.selectedDate}T${data.selectedTime}`
+      ).getTime(),
+      barberId: data.selectedBarber,
+      serviceId: data.selectedService,
     });
 
     navigate("/success");
@@ -118,7 +56,7 @@ function Form() {
       </div>
       <form
         className="bg-dark-blue p-6 flex flex-col items-center w-full md:w-max justify-center space-y-4  mb-6 md:mt-0 md:mb-0"
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
       >
         <h3 className="text-white text-2xl  text-tracking-xl font-squada text-center">
           BOOK YOUR APPOINTMENT
@@ -127,47 +65,70 @@ function Form() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
             <div>
               <input
+                {...register("firstName", {
+                  required: "First name is required",
+                })}
                 type="text"
-                name="firstName"
                 placeholder="First Name"
                 className="bg-white rounded-sm font-roboto p-2 w-full"
               />
+              {errors.firstName && (
+                <p className="text-red-500 font-roboto">
+                  {errors.firstName.message}
+                </p>
+              )}
             </div>
             <div>
               <input
+                {...register("lastName", {
+                  required: "Last name is required",
+                })}
                 type="text"
-                name="lastName"
                 placeholder="Last Name"
                 className="bg-white rounded-sm font-roboto p-2 w-full"
               />
+              {errors.lastName && (
+                <p className="text-red-500 font-roboto">
+                  {errors.lastName.message}
+                </p>
+              )}
             </div>
           </div>
-          {errors.name && (
-            <p className="text-red-500 font-roboto">{errors.name}</p>
-          )}
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
           <div>
             <input
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Please enter a valid email",
+                },
+              })}
               type="email"
-              name="email"
               placeholder="Email"
               className="bg-white rounded-sm font-roboto p-2 w-full"
             />
             {errors.email && (
-              <p className="text-red-500 font-roboto">{errors.email}</p>
+              <p className="text-red-500 font-roboto">{errors.email.message}</p>
             )}
           </div>
           <div>
             <input
+              {...register("phone", {
+                required: "Phone number is required",
+                pattern: {
+                  value: /^(\+386\s?[1-7]\s?\d{3}\s?\d{2}\s?\d{2})$/,
+                  message: "Please enter a valid phone number",
+                },
+              })}
               type="text"
-              name="phone"
               placeholder="Slovenian Contact Number"
               className="bg-white rounded-sm font-roboto p-2 w-full"
             />
             {errors.phone && (
-              <p className="text-red-500 font-roboto">{errors.phone}</p>
+              <p className="text-red-500 font-roboto">{errors.phone.message}</p>
             )}
           </div>
         </div>
@@ -175,31 +136,40 @@ function Form() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
           <div>
             <BarbersDropdown
-              onSelect={(barberId) => setSelectedBarber(barberId)}
+              onSelect={(barberId) => setValue("selectedBarber", barberId)} // Set selected barber in form state
             />
-            {errors.barber && (
-              <p className="text-red-500 font-roboto">{errors.barber}</p>
+            {errors.selectedBarber && (
+              <p className="text-red-500 font-roboto">
+                {errors.selectedBarber.message}
+              </p>
             )}
           </div>
           <div>
             <ServicesDropdown
-              onSelect={(serviceId) => setSelectedService(serviceId)}
+              onSelect={(serviceId) => {
+                setValue("selectedService", serviceId); // Set selected service
+              }}
               setPrice={setServicePrice}
             />
-            {errors.service && <p className="text-red-500">{errors.service}</p>}
+            {errors.selectedService && (
+              <p className="text-red-500">{errors.selectedService.message}</p>
+            )}
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
           <div>
             <input
+              {...register("selectedDate", {
+                required: "Please select a date",
+              })}
               type="date"
-              onChange={(e) => setSelectedDate(e.target.value)}
               className="bg-white rounded-sm font-roboto text-light-gray p-2 w-full"
               min={new Date().toISOString().split("T")[0]}
-              value={selectedDate}
             />
-            {errors.date && (
-              <p className="text-red-500 font-roboto">{errors.date}</p>
+            {errors.selectedDate && (
+              <p className="text-red-500 font-roboto">
+                {errors.selectedDate.message}
+              </p>
             )}
           </div>
           <div>
@@ -207,10 +177,12 @@ function Form() {
               barberId={selectedBarber}
               selectedDate={new Date(selectedDate)}
               serviceDuration={30}
-              onSelect={setSelectedTime}
-            />{" "}
-            {errors.time && (
-              <p className="text-red-500 roboto-slab-medium">{errors.time}</p>
+              onSelect={(time) => setValue("selectedTime", time)} // Set selected time
+            />
+            {errors.selectedTime && (
+              <p className="text-red-500 roboto-slab-medium">
+                {errors.selectedTime.message}
+              </p>
             )}
           </div>
         </div>
